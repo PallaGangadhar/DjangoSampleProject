345d81f9e30202cb5ecfc677f7f9c58bcfe75745
pallagangadhar <ganga11497@gmail.com>
2023-09-13 18:02:56 +0530

    "changes"

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # # Media files settings
 # MEDIA_URL = '/media/'
